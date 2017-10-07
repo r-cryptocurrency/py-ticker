438af0b9b890a9061421e31b0aa656ab75ca2c30
root <root@monerobux.localdomain>
2017-10-07 15:55:01 +0000

    Update before major changes

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import imggen
 import praw
+from datetime import datetime
 from userpw import *
 
-tickerimg = imggen.imggen()
+try:
+    tickerimg = imggen.imggen()
+    with open('pyticker.log', 'a') as f:
+        f.write("{} - successfully created ticker image\n".format(str(datetime.now())))
+except:
+    with open('pyticker.log', 'a') as f:
+        f.write("{} - failed to created ticker image\n".format(str(datetime.now())))
 
-reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
-                     username=username, password=password,
-                     user_agent=user_agent)
 
-print(reddit.user.me())
-sub = reddit.subreddit('cryptocurrency')
-# print(sub.title)
-ss = sub.stylesheet()
-sstext = ss.stylesheet
-sub.stylesheet.upload("ticker", "img/temp/ticker.png")
-sub.stylesheet.update(sstext)
+try:
+    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
+                         username=username, password=password,
+                         user_agent=user_agent)
+    
+    print(reddit.user.me())
+    sub = reddit.subreddit('cryptocurrency')
+    # print(sub.title)
+    ss = sub.stylesheet()
+    sstext = ss.stylesheet
+    sub.stylesheet.upload("ticker", "img/temp/ticker.png")
+    sub.stylesheet.update(sstext)
+    with open('pyticker.log', 'a') as f:
+        f.write("{} - successfully updated reddit\n".format(str(datetime.now())))
+except:
+    with open('pyticker.log', 'a') as f:
+        f.write("{} - failed to update reddit\n".format(str(datetime.now())))
