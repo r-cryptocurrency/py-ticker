69762b1e4e7d49bb289f866bd87077cec4b41ff8
root <root@monerobux.localdomain>
2018-07-31 03:38:46 +0000

    update to new css

--- imggen.py
+++ imggen.py
@@ -12,66 +12,17 @@ def trim(im):
         # return im.crop(bbox)
         return im.crop((bbox[0], 0, bbox[2], 18))
 
-def imggen():
-    cmcdata = cmc.getcmc()
-    # font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/arial.ttf",16)
+def imggen(cmcdata):
 
-    spacing = 60
-    logosize = 16
-    bufferspace = 60
-
-    font = ImageFont.truetype("Ubuntu-B.ttf",16)
-
-    coinimglist = []
-    for root, dirs, files in os.walk("img/coins/"):
-        for file in files:
-            if file.endswith(".png"):
-                 coinimglist.append(os.path.join(root, file))
-    # print(coinimglist)
-    widthlist = []
-    toptenlist = []
-
-    leaderimg = Image.new("RGBA", (400,18), (200,240,255,255))
-    draw = ImageDraw.Draw(leaderimg)
-    draw.text((0,0), "Rank  Coin  Marketcap  Price(USD)", (0,0,0), font=font)
-    leaderimg = trim(leaderimg)
-    widthlist.append(leaderimg.size[0])
-    leaderimg.save("img/temp/leader.png")
+    tickerimg = Image.new("RGBA", (64,640), (255,255,255,255))
 
     for idx,val in enumerate(cmcdata):
-        if idx == 0:
-            img = Image.new("RGBA", (400,18), (200,255,200,255))
-        else:
-            img = Image.new("RGBA", (400,18), (255,255,255,255))
-        draw = ImageDraw.Draw(img)
-        text = "#{}  {}  ${:,.0f}  ${:.2f}".format(val['rank'], val['symbol'], float(val['market_cap_usd']), float(val['price_usd']))
-        draw.text((0,0), text, (0,0,0), font=font)
-        img = trim(img)
-        img.save("img/temp/{}.png".format(val['id']))
-        widthlist.append(img.size[0])
-        toptenlist.append(val['id'])
-
-    x_offset = 0
-    tickerimg = Image.new("RGBA", (sum(widthlist)+10*(spacing+logosize)+bufferspace,18), (255,255,255,255))
-    tickerimg.paste(leaderimg, (x_offset, 0))
-    x_offset += widthlist[0]
-
-    for idx,val in enumerate(toptenlist):
-        coinimg = Image.open("img/coins/unknown.png")
-        for i in coinimglist:
-        # print(cmcdata[idx]['id'], coinimglist[idx])
-            if cmcdata[idx]['id'] in i:
-                coinimg = Image.open("img/coins/{0}.png".format(cmcdata[idx]['id']))
-        textimg = Image.open("img/temp/{0}.png".format(cmcdata[idx]['id']))
-        # arr = np.array(coinimg)
-        # print(arr)
-        # coinimgbg = Image.new("RGBA", (16,16), (255,255,255,255))
-        # print("Size of coinimg is {}, and size of coinimgbg is {}".format(coinimg.size, coinimgbg.size))
-        # print("Mode of coinimg is {}, and mode of coinimgbg is {}".format(coinimg.mode, coinimgbg.mode))
-        # coinimg = Image.alpha_composite(coinimgbg, coinimg)
-        tickerimg.paste(coinimg, (x_offset+spacing, 1))
-        tickerimg.paste(textimg, (x_offset+spacing+logosize, 0))
-        x_offset += widthlist[idx+1] + 76
+        coinimg = Image.open("img/newcoin/unknown.png")
+        try: 
+            coinimg = Image.open("img/newcoin/{}.png".format(val["id"]))
+        except:
+            pass 
+        tickerimg.paste(coinimg, (0, idx*64))
     tickerimg.save("img/temp/ticker.png")
     print("Ticker image size = {}".format(tickerimg.size))
     return tickerimg
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import imggen
+import cmc
 import praw
+import re
 from datetime import datetime
 from userpw import *
 
 try:
-    tickerimg = imggen.imggen()
+    cmcdata = cmc.getcmc()
+    cmctext = ''
+    for i in cmcdata:
+        print(i["id"])
+        cmctext += "{0}. **{1} ({2}) - ${3:.2f}B - ${4:.2f}**\n".format(i["rank"], i["name"], i["symbol"], float(i["market_cap_usd"])/1e9, float(i["price_usd"]))
+    cmctext = cmctext[:-1]
+    print(cmctext)
+    with open('pyticker.log', 'a') as f:
+        f.write("{} - successfully got ticker data\n".format(str(datetime.now())))
+except:
+    with open('pyticker.log', 'a') as f:
+        f.write("{} - failed to get ticker data\n".format(str(datetime.now())))
+
+try:
+    tickerimg = imggen.imggen(cmcdata)
     with open('pyticker.log', 'a') as f:
         f.write("{} - successfully created ticker image\n".format(str(datetime.now())))
 except:
@@ -19,11 +35,17 @@ try:
     
     print(reddit.user.me())
     sub = reddit.subreddit('cryptocurrency')
+    # sub = reddit.subreddit('cryptocurrencyadmin')
     # print(sub.title)
     ss = sub.stylesheet()
     sstext = ss.stylesheet
-    sub.stylesheet.upload("ticker", "img/temp/ticker.png")
+    settings = praw.models.reddit.subreddit.SubredditModeration(sub).settings()
+    sidebar_contents = settings['description']
+    sidebar_contents = re.sub(r'(1\. \*\*Bitcoin.+\n.+\n.+\n.+\n.+\n.+\n.+\n.+\n.+\n.+)', cmctext, sidebar_contents)
+    # sub.stylesheet.upload("ticker", "img/temp/ticker.png")
+    sub.stylesheet.upload("crypto-top10", "img/temp/ticker.png")
     sub.stylesheet.update(sstext)
+    praw.models.reddit.subreddit.SubredditModeration(sub).update(description=sidebar_contents)
     with open('pyticker.log', 'a') as f:
         f.write("{} - successfully updated reddit\n".format(str(datetime.now())))
 except:
